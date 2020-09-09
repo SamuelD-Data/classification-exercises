b00900ac9920fd6ddd9ee25df6f440364b533345
Samuel Davila <samuelrdavila@gmail.com>
2020-09-09 15:07:03 -0500

    updated file with imports

--- acquire.py
+++ acquire.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 # Make a function named get_titanic_data that returns the titanic data from the codeup data science database 
 # as a pandas data frame. 
 # Obtain your data from the Codeup Data Science Database.
