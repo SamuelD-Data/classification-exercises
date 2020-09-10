e5db0450d29daac4cd53740fb8dda633b5c37a6f
Samuel Davila <samuelrdavila@gmail.com>
2020-09-10 10:56:05 -0500

    updated get titanic code with alternate scalar function

--- prepare.py
+++ prepare.py
@@ -10,8 +10,8 @@ def prep_iris(idf):
     dummy_df = pd.get_dummies(idf['species'], dummy_na = False)
     idf = pd.concat([idf, dummy_df], axis=1)
     return idf
-    
-# prep titanic function
+
+# prep titanic data
 def prep_titanic(tdf):
     from sklearn.preprocessing import MinMaxScaler
     from sklearn.impute import SimpleImputer
@@ -23,14 +23,9 @@ def prep_titanic(tdf):
     dummy_df = pd.get_dummies(tdf['embarked'], dummy_na = False)
     tdf = pd.concat([tdf, dummy_df], axis=1)
     scaler = MinMaxScaler()
-    scaler.fit(tdf[['age']])
-    age_scaler = scaler.transform(tdf[['age']])
-    tdf['age_scaled'] = age_scaler
-    scaler.fit(tdf[['fare']])
-    fare_scaler = scaler.transform(tdf[['fare']])
-    tdf['fare_scaled'] = fare_scaler 
+    scaler = MinMaxScaler()
+    tdf[['age', 'fare']] = scaler.fit_transform(tdf[['age', 'fare']])
     imputer = SimpleImputer(strategy = 'most_frequent')
     imputer = imputer.fit(tdf[['age']])
     tdf[['age']] = imputer.transform(tdf[['age']])
     return tdf
-
